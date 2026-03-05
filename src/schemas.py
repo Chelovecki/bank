a60d1d38c44ecf1ae86b0faa99c35a37f069b74b
chelovecki <benkozlovv@gmail.com>
2026-03-05 17:13:53 +0300

    add: schemas

---
+++ src/schemas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict
+
+from db_models import OrderPaymentStatus, PaymentStatus, PaymentType
+
+
+class BaseSchema(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+        extra='forbid'
+    )
+
+
+class PaymentSchema(BaseSchema):
+    id: int
+    amount: Decimal
+    type: PaymentType
+    status: PaymentStatus
+    bank_payment_id: str | None
+    created_at: datetime
+    order_id: int
+
+
+class PaymentCreateSchema(BaseSchema):
+    amount: Decimal
+    type: PaymentType
+
+
+class OrderSchema(BaseSchema):
+    id: int
+    amount: Decimal
+    payment_status: OrderPaymentStatus
+    created_at: datetime
+
+
+class OrderWithPaymentsSchema(OrderSchema):
+    payments: list[PaymentSchema]
